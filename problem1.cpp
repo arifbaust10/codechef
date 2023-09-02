13b424c797cacd9a9e636f356fc1d1023651d091
Md. Ariful Islam <111891900+arifbaust10@users.noreply.github.com>
2023-09-02 15:24:15 +0600

    Basic math - Addition and multiplication
    https://www.codechef.com/learn/BP00BC05_V2/problems/BMCV201

---
+++ problem1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+ int t;
+ cin>>t;
+ while(t--)
+  { 
+   int A,B,S,P;
+   cin>>A>>B;;
+   S = A + B;               
+   P = A * B;               
+   cout<<S<<" "<<P<<endl;
+   }
+ return 0;
+}
