2c2103a22e6d4e001fb3c9f8cfe1d00117e95443
Md. Ariful Islam <111891900+arifbaust10@users.noreply.github.com>
2023-12-07 18:53:05 +0600

    Fitness.cpp
    https://www.codechef.com/practice/course/basic-math-cpp/PCPPBM01/problems/FIT

---
+++ Fitness.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,number;
+    cin>>t;
+    for(int i=0;i<t;i++){
+     
+        cin>>number;
+        cout<<number*2*5<<endl;
+    }
+}
